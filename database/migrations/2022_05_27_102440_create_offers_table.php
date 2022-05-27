e376ea5862676b42715d659ce977bf1a9c66532e
BlauensteinerFlorian <florian.blauen@gmail.com>
2022-05-27 14:45:47 +0200

    created migrations for subjects and offers

---
+++ database/migrations/2022_05_27_102440_create_offers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('state')->default('open');
+            $table->string('start_time');
+            $table->string('end_time');
+            $table->string('date');
+
+            // Foreign Keys
+            $table->foreignId('student_id')->nullable()->constrainend('users')->nullonDelete();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
