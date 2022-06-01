ebfef032158134084bb915d5fa646099495d5a27
Florian Blauensteiner <florian.blauen@gmail.com>
2022-06-01 15:02:32 +0200

    updated getPendingReqeustsByOfferId

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -24,7 +24,7 @@ class RequestController extends Controller
     }
 
     public function getPendingRequestsByOfferId(string $id): JsonResponse{
-        $requestmodel = RequestModel::where("offer_id", $id)->get();
+        $requestmodel = RequestModel::with("offer", "user")->where("offer_id", $id)->get();
         return response()->json($requestmodel, 200);
     }
 
